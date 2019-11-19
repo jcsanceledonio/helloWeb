ce962a35f3853f223ceeaef2b1a90d6866875be2
andonishiraiwa <Curso@Usuario-TOSH01.ipartekaula.com>
2019-11-19 12:09:02 +0100

    segundo commit

--- src/com/ipartek/Holamundo.java
+++ src/com/ipartek/Holamundo.java
@@ -15,7 +15,7 @@ public class Holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola mundo!!");
+		System.out.println("Hola mundoooooooo!!");
 
 	}
 
